e8568c0784cbdbe15ebad6b94139ddfbed1f1651
Mehdi KH <khairandishmehdi@gmail.com>
2025-12-17 00:11:47 +0330

    Add advance filtering(with operator support like gte,lte,in) sort select pagination to project

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -4,10 +4,77 @@ const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  console.log(req.query);
+  let query;
+  const reqQuery = { ...req.query };
+  const keyArray = Object.keys(reqQuery);
+  keyArray.forEach((item) => {
+    if (
+      item.endsWith("$lte") ||
+      item.endsWith("$lt") ||
+      item.endsWith("$gt") ||
+      item.endsWith("$gte") ||
+      item.endsWith("$in")
+    ) {
+      const splitedItem = item.split(".");
+      reqQuery[[splitedItem[0]]] = {
+        [splitedItem[1]]: reqQuery[item],
+      };
+      delete reqQuery[item];
+    }
+  });
+
+  const removedFields = ["select", "sort", "page", "limit"];
+
+  removedFields.forEach((item) => {
+    delete reqQuery[item];
+  });
+  console.log(reqQuery);
+  query = Bootcamp.find(reqQuery);
+
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Add paggination
+
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments(reqQuery);
+
+  query = query.skip(startIndex).limit(limit);
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  const bootcamps = await query;
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
